18b70738f418763f76bf30cc6a7eb85876724a76
Wassim AZIRAR <wassim.azirar@gmail.com>
2017-01-23 02:26:11 +0100

    added karma unit testing and reformat code using tabs instead of spaces

--- app/pages/list/list.component.ts
+++ app/pages/list/list.component.ts
@@ -1,8 +1,8 @@
-import { Component } from "@angular/core";
+import {Component} from "@angular/core";
 
 @Component({
-  selector: "list",
-  templateUrl: "pages/list/list.html",
-  styleUrls: ["pages/list/list-common.css", "pages/list/list.css"]
+    selector: "list",
+    templateUrl: "pages/list/list.html"
 })
-export class ListComponent {}
+export class ListComponent {
+}
--- app/pages/login/login.component.ts
+++ app/pages/login/login.component.ts
@@ -1,52 +1,52 @@
-import { Component } from "@angular/core";
-import { User } from "../../shared/user/user";
-import { UserService } from "../../shared/user/user.service";
-import { Router } from "@angular/router";
+import {Component} from "@angular/core";
+import {User} from "../../shared/user/user";
+import {UserService} from "../../shared/user/user.service";
+import {Router} from "@angular/router";
 
 @Component({
-  selector: "my-app",
-  providers: [UserService],
-  templateUrl: "pages/login/login.html",
-  styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
+    selector: "my-app",
+    providers: [UserService],
+    templateUrl: "pages/login/login.html",
+    styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
 })
 export class LoginComponent {
-	user: User;
-	isLoggingIn = true;
+    user: User;
+    isLoggingIn = true;
 
     constructor(private router: Router, private userService: UserService) {
-		this.user = new User();
-		this.user.email = "user@nativescript.org";
-		this.user.password = "password";
-	}
+        this.user = new User();
+        this.user.email = "user@nativescript.org";
+        this.user.password = "password";
+    }
 
-	submit() {
-	  if (this.isLoggingIn) {
-		this.login();
-	  } else {
-		this.signUp();
-	  }
-	}
+    submit() {
+        if (this.isLoggingIn) {
+            this.login();
+        } else {
+            this.signUp();
+        }
+    }
 
-	login() {
-	this.userService.login(this.user)
-	  .subscribe(
-		() => this.router.navigate(["/list"],
-		(error) => alert("Unfortunately we could not find your account.")
-	  );
-	}
+    login() {
+        this.userService.login(this.user)
+            .subscribe(
+                () => this.router.navigate(["/list"]),
+                (error) => alert("Unfortunately we could not find your account.")
+            );
+    }
 
-	signUp() {
-	  this.userService.register(this.user)
-		.subscribe(
-		  () => {
-			alert("Your account was successfully created.");
-			this.toggleDisplay();
-		  },
-		  () => alert("Unfortunately we were unable to create your account.")
-		);
-	}
+    signUp() {
+        this.userService.register(this.user)
+            .subscribe(
+                () => {
+                    alert("Your account was successfully created.");
+                    this.toggleDisplay();
+                },
+                () => alert("Unfortunately we were unable to create your account.")
+            );
+    }
 
-	toggleDisplay() {
-	this.isLoggingIn = !this.isLoggingIn;
-	}
+    toggleDisplay() {
+        this.isLoggingIn = !this.isLoggingIn;
+    }
 }
